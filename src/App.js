181da28df1b7aa67f579e1cbb9c7637aefe1056d
maekawamikako <mikankounya@gmail.com>
2020-12-31 01:46:26 +0900

    URL、日本標準時、実施時間の追加、変更。CSS追加

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ const App = () => {
 
   useEffect(() =>{ 
     getInformation()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   // 情報取得
@@ -27,35 +28,35 @@ const App = () => {
             return 1;
         }
       })
-      const changedObject = objectChange(result.data)
-      setContestsList(changedObject)
-      setDisplayList(changedObject)
+      objectChange(result.data)
+      setContestsList(result.data)
+      setDisplayList(result.data)
     } catch (error) {
       // リクエストに失敗した時の処理
       console.log('error!!')
     }
   }
 
-  const objectChange = (e) => {
-    return e.map((time) => {
-      time.start = new Date(time.start_epoch_second * 1000).toString() // => Wed Sep 04 2019 12:03:35 GMT+0900 (日本標準時)
-      time.finish = new Date((time.start_epoch_second + time.contest.duration_second) * 1000).toString()
-      time.duration_second /= 60 // => 試験時間(分)
-      time.url = 'https://atcoder.jp/contests/' + e.id
-      console.log(time.start_epoch_second, time.duration_second)
+  const objectChange = (list) => {
+    return list.map(contest => {
+      contest.start = new Date(contest.start_epoch_second * 1000).toString()
+      contest.start = contest.start.replace(' GMT+0900 (日本標準時)', '')
+      contest.duration_second =  contest.duration_second / 60 + '分'// => 試験時間(分)
+      contest.url = 'https://atcoder.jp/contests/' + contest.id
+      return contest
     })
   }
 
   // もっと見る
   const onClickShowMore = () => {
-    setDisplayCount(displayCount + 10)
+    setDisplayCount(displayCount => displayCount + 10)
   }
 
   // 種類
   const typeHandleChange = (e) => {
     setType(e.target.value)
     setDisplayList(contestsList.filter((contest) => {
-      if(contest.id.indexOf(e.target.value) !== -1) return true
+      return contest.id.indexOf(e.target.value) !== -1
     }))
     console.log(e.target.value, sort)
   }
@@ -71,7 +72,7 @@ const App = () => {
     const line = contestsList.filter((contest) => (
       // キーワードが含まれていればtrueを返す
       contest.id.toLowerCase().indexOf(e.target.value) >= 0
-      || contest.start_epoch_second.toString().indexOf(e.target.value) >= 0
+      || contest.start.toString().indexOf(e.target.value) >= 0
       || contest.duration_second.toString().indexOf(e.target.value) >= 0
       || contest.title.toLowerCase().indexOf(e.target.value) >= 0
     ));
@@ -93,7 +94,7 @@ const App = () => {
   }
   const sortFilter = (e) => {
     if (e === 'newer') {
-      setDisplayList(displayList.sort ((a, b) => {
+      setDisplayList(displayList => displayList.sort ((a, b) => {
         if (a.start_epoch_second > b.start_epoch_second) {
             return -1;
         } else {
@@ -101,7 +102,7 @@ const App = () => {
         }
       }))
     }if (e === 'older') {
-      setDisplayList(displayList.sort ((a, b) => {
+      setDisplayList(displayList => displayList.sort ((a, b) => {
         if (a.start_epoch_second < b.start_epoch_second) {
             return -1;
         } else {
@@ -109,7 +110,7 @@ const App = () => {
         }
       }))
     }if (e === 'abcSort') {
-      setDisplayList(displayList.sort ((a, b) => {
+      setDisplayList(displayList => displayList.sort ((a, b) => {
         if (a.title < b.title) {
             return -1;
         } else {
@@ -123,50 +124,80 @@ const App = () => {
     <div className="App">
 
       <div className='header'>
-        <h1>Atcoder Contests Information</h1>
+        <h1 className='headerLogo'>Atcoder Contests Information</h1>
+
+        <div className="formats">
+          <div className='format'>
+            <select　onChange={typeHandleChange} value={type}>
+              <option value=''>ALL</option>
+              <option value='abc'>ABC</option>
+              <option value='arc'>ARC</option>
+              <option value='agc'>AGC</option>
+              <option value='past'>PAST</option>
+              <option value='joi'>JOI</option>
+              <option value='jag'>JAG</option>
+            </select>
+          </div>
+          <div className='format'>
+            <select　onChange={displayCountHandleChange} value={displayCount}>
+              <option value='10'>10件表示</option>
+              <option value='50'>50件表示</option>
+              <option value='100'>100件表示</option>
+              <option value= {displayList} >全件表示</option>
+            </select>
+          </div>
+          <div className='format'>
+            <select onChange={sortHandleChange} value={sort}>
+              <option value='newer'>新しい順</option>
+              <option value='older'>古い順</option>
+              <option value='abcSort'>五十音順</option>
+            </select>
+          </div>
+          <div className='format'>
+            <label class="formatFocus">
+            <input
+              className='textFormat'
+              type = 'text'
+              value = {keyword}
+              placeholder = 'keyword'
+              onChange= {(e) => {keywordChange(e)}} 
+            />
+            </label>
+	          <span class="focus_line"></span>
+          </div>
+        </div>
       </div>
 
       <div className='main'>
-
-        <div id="format">
-          <select　onChange={typeHandleChange} value={type}>
-            <option value=''>ALL</option>
-            <option value='abc'>ABC</option>
-            <option value='arc'>ARC</option>
-            <option value='agc'>AGC</option>
-            <option value='past'>PAST</option>
-            <option value='joi'>JOI</option>
-            <option value='jag'>JAG</option>
-          </select>
-          <input
-            type='text'
-            value = {keyword}
-            onChange= {(e) => {keywordChange(e)}} 
-          />
+        <div className='title'>
+          <h2>Information about the contests</h2>
         </div>
-          
-        <h2>Information about the contests</h2>
-
-        <select　onChange={displayCountHandleChange} value={displayCount}>
-          <option value='10'>10件表示</option>
-          <option value='50'>50件表示</option>
-          <option value='100'>100件表示</option>
-          <option value= {displayList} >全件表示</option>
-        </select>
-        <select onChange={sortHandleChange} value={sort}>
-          <option value='newer'>新しい順</option>
-          <option value='older'>古い順</option>
-          <option value='abcSort'>五十音順</option>
-        </select>
 
         { displayList.map((contest, index) => {
           if (index >= displayCount) return null
           return (
-            <div>
-              {contest.id}
-              {contest.start_epoch_second}
-              {contest.duration_second}
-              {contest.title}
+            <div className="contestsTable">
+              <h3><a href="{contest.url}" className='contestTitle'>{contest.title}</a></h3>
+              <table>
+                <tbody>
+                    <tr>
+                        <th>開始時刻</th>
+                        <td>{contest.start}</td>
+                    </tr>
+                    <tr>
+                        <th>コンテスト時間</th>
+                        <td>{contest.duration_second}</td>
+                    </tr>
+                    <tr>
+                        <th>レート変化</th>
+                        <td>{contest.rate_change}</td>
+                    </tr>
+                    <tr>
+                        <th>URL</th>
+                        <td><a href="{contest.url}">{contest.url}</a></td>
+                    </tr>
+                </tbody>
+              </table>
             </div>
           )
         })}
